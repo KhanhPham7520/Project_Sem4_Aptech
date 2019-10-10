479b6dfb73b12f5ac1315d69d5225a924d0be30c
PhamPhanNhatKhanh <>
2019-10-10 22:32:03 +0700

    [10-10-2019] [10:31 PM] Init project

---
+++ Project_Sem4_Aptech/src/main/java/com/freebelieve/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.freebelieve.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	
+	@GetMapping
+	public String returnHome() {
+		return "home";
+	}
+}
